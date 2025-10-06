1f608fc10f157296255bbb9fb8eae7d7fc26ba49
Chirag Shewale <chiragshewale995@gmail.com>
2025-10-06 07:49:33 +0530

    Update variables.tf

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -4,3 +4,7 @@ variable "resource_group_name" { type = string }
 variable "vnet_address_space" { type = list(string) }
 variable "subnet_name" { type = string }
 variable "subnet_address_prefix" { type = string }
+variable "nsg_id" {
+  type        = string
+  description = "Network Security Group ID to associate with subnet"
+}
